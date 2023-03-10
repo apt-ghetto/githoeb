8019f09bf2f8c65594be9b1e7c361b7e3f74d8ff
apt-ghetto <apt-ghetto@protonmail.com>
2023-03-10 15:34:23 +0100

    EMACS: Konfiguration total überarbeitet
      * Anstelle der ~/.emacs jetzt ~/.emacs.d/init.el
      * init.el erledigt das nötigste
      * Dokumentation der Einstellungen in ~/.emacs.d/config.org
      * Daraus wird dann die ~/.emacs.d/config.el erstellt

---
+++ emacs/init.el
@@ -0,0 +1,37 @@
+;;; init.el --- Summary  -*- coding: utf-8; lexical-binding: t; -*-
+
+;;; Commentary:
+
+;; Emacs configuration starting point of apt-ghetto, heavily inspired by others.
+;; The main part of the configuration is in the ~/.emacs.d/config.org file.
+(require 'org)
+
+;;; Code:
+(org-babel-load-file
+ (expand-file-name "config.org"
+		   user-emacs-directory))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(custom-enabled-themes '(manoj-dark))
+ '(doc-view-continuous nil)
+ '(erc-nick "apt-ghetto")
+ '(erc-server "irc.libera.chat")
+ '(global-linum-mode t)
+ '(package-selected-packages
+   '(nasm-mode yasnippet-snippets lsp-treemacs lsp-ivy lsp-ui lsp-mode company-ctags edit-server auctex company-auctex impatient-mode counsel-etags markdown-mode flycheck magit modern-cpp-font-lock elpy use-package))
+ '(safe-local-variable-values '((TeX-close-quote . "\"'") (TeX-open-quote . "\"`"))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(provide 'init)
+;;; init.el ends here
