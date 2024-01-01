3d88d9f710e4cf7236734776f514cd48f83bb1eb
apt-ghetto <apt-ghetto@protonmail.com>
2024-01-01 17:29:46 +0100

    EMACS: Customvariablen verschoben
    Die Variablen, die von Custom verwaltet werden, in eine eigene Datei
    verschoben.

--- emacs/init.el
+++ emacs/init.el
@@ -1,4 +1,4 @@
-;;; .init.el --- Summary  -*- coding: utf-8; lexical-binding: t; -*-
+;;; .init.el --- Summary -*- coding: utf-8; lexical-binding: t; -*-
 
 ;;; Commentary:
 
@@ -10,29 +10,6 @@
 (org-babel-load-file
  (expand-file-name "config.org"
 		   user-emacs-directory))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(column-number-mode t)
- '(custom-enabled-themes '(manoj-dark))
- '(doc-view-continuous t)
- '(erc-join-buffer 'window-noselect)
- '(erc-nick "apt-ghetto")
- '(erc-server "irc.libera.chat")
- '(global-display-line-numbers-mode 1)
- '(package-selected-packages
-   '(clang-format nasm-mode yasnippet-snippets lsp-treemacs lsp-ivy lsp-ui lsp-mode company-ctags edit-server auctex company-auctex impatient-mode counsel-etags markdown-mode flycheck magit modern-cpp-font-lock elpy use-package))
- '(safe-local-variable-values '((TeX-close-quote . "\"'") (TeX-open-quote . "\"`"))))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
 (provide 'init)
 ;;; init.el ends here
